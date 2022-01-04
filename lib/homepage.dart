a3ffe67cd63bf12893ce418339a338c193f4db68
unknown <ahmalik_b20@it.vjti.ac.in>
2022-01-04 21:41:42 +0530

    firstday

---
+++ lib/homepage.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+class homepage extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body:Center(
+        child:Container(
+    child:Text("this is just beggining")
+
+    )
+    ),
+    drawer: Drawer()
+
+
+
+
+
+    );
+  }
+}
